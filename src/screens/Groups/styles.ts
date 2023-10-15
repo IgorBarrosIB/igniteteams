76a404e5ad869843c924cf2e6c6d5cc9c18752ee
Igor Barros <igorbarrosib@gmail.com>
2023-10-15 16:17:21 -0300

    adicionando component de loading

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -5,3 +5,8 @@ export const Container = styled.View`
    background-color: ${({theme}) => theme.COLORS.GRAY_700};
    padding: 24px; /* espaçamento interno */
 `;
+
+export const TextLogin = styled.Text`
+   font-size: ${({theme}) => theme.FONT_SIZE.MD};
+   font-family: ${({theme}) => theme.FONT_FAMILY.Roboto_700Bold};
+`;
